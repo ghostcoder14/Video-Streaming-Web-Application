c3ae77eabd7b29e4dc602f8d727bf3b87bdeb208
Aakash Ojha <oaakash14@gmail.com>
2024-03-24 14:55:32 +0530

    modified

--- src/index.js
+++ src/index.js
@@ -9,26 +9,4 @@ mongodb()
 
 
 
-/*
-import express from "express";
-import { error } from "console";
-const app = express()
 
-(async() => {
-    try {
-        await mongoose.connect(`${process.env.MONGODB_URI} / ${DB_NAME}`);
-        app.on("error" , (error) => {
-            console.log("ERROR" ,   error);
-            throw  error; ;
-        })
-
-        app.listen(process.env.PORT , ()=>{
-            console.log(`app is listening at ${process.env.PORT}`);
-        })
- 
-         
-    } catch (error) {
-        console.error("ERROR :" , error);
-    }
-})()
-*/
